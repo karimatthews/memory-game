5699795033b28c2f6a33128227344b1d93c661f4
Kari Matthews <karimatthews3@gmail.com>
2017-05-09 20:39:34 +1000

    Colour scheme and new class for clicked buttons

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
+function shuffle(array) {
+    let counter = array.length;
 
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
 
 
 
@@ -10,9 +27,18 @@
     //define number of rows/columns
     var rowNum = Math.sqrt(squareNum);
 
+
+
+    //create function to run when the button is clicked
+    function showValue(i) {
+      return function() {
+        cards[i].innerHTML = numbers[i];
+        cards[i].className = "clickedCard"
+      }
+    }
     //create a list of numbers to use
     numbers = [];
-    for (i = 0; i < squareNum * 2; i=i+2) {
+    for (i = 0; i < squareNum ; i=i+2) {
       if (i==0) {
         numbers[i] = 1;
         numbers[i+1] = 1;
@@ -23,20 +49,32 @@
       }
     }
 
-
+    randomNumbers = shuffle(numbers);
 
     cards = [];
-    for (i=0; i<squareNum; i++) {
+    var gameDiv = document.getElementById('game');
+    divs = [];
+    divs[0] = document.createElement("div")
+    divs[0].className = "row";
+    gameDiv.appendChild(divs[0])
+
+
+    //Run loop to build grid
+    for (i=0, j=0; i<squareNum; i++) {
       cards[i] = document.createElement("button");        // Create a <button> element
       cards[i].className = "card";
-      //put text in cards
-      cards[i].innerHTML = Math.round(Math.random()*10);
-      var gameDiv = document.getElementById('game');
-      gameDiv.appendChild(cards[i]);
-      //create rows
+      //call function when button is clicked
+      cards[i].onclick = showValue(i);
+
+      //add card to the row
+      divs[j].appendChild(cards[i]);
+
+      //create new row when needed
       if (Number.isInteger((i+1)/rowNum)) {
-        lineBreak = document.createElement("br")
-        gameDiv.appendChild(lineBreak)
+        j = j++
+        divs[j] = document.createElement("div")
+        divs[j].className = "row";
+        gameDiv.appendChild(divs[j])
       }
     }
   }
